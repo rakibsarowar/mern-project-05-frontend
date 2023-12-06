b493ee82603ea4b8ab604f0cbddf8da3579f20f7
rakibsarowar <rakibsarowar@gmail.com>
2023-12-06 20:17:03 +0600

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,7 @@ function App() {
   return (
     <>
       <div>
-        <button className="btn btn-active">Default</button>
-        <button className="btn btn-active btn-neutral">Neutral</button>
-        <button className="btn btn-active btn-primary">Primary</button>
-        <button className="btn btn-active btn-secondary">Secondary</button>
-        <button className="btn btn-active btn-accent">Accent</button>
-        <button className="btn btn-active btn-ghost">Ghost</button>
-        <button className="btn btn-active btn-link">Link</button>
+
       </div>
     </>
   )
