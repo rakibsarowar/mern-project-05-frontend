b69ed60c599720c3957636d14f3d07bf727753bd
rakibsarowar <rakibsarowar@gmail.com>
2023-12-06 21:51:57 +0600

    Use swiper JS to create a category Swiper01

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Category from "./Category/Category";
 import Hero from "./Hero/Hero";
 
 const Home = () => {
     return (
         <div>
             <Hero></Hero>
+            <Category></Category>
         </div>
     );
 };
