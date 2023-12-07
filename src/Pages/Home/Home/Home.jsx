fa344d68dbd968ee093dae60a37ced2ff3498920
rakibsarowar <rakibsarowar@gmail.com>
2023-12-07 21:12:54 +0600

    added paralex

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,9 @@
 import { Helmet } from "react-helmet-async";
-import Category from "./Category/Category";
-import Featured from "./Featured/Featured";
-import Hero from "./Hero/Hero";
-import PopularMenu from "./PopularMenu/PopularMenu";
-import Testimonials from "./Testimonials/Testimonials";
+import Category from "../Category/Category";
+import Featured from "../Featured/Featured";
+import Hero from "../Hero/Hero";
+import PopularMenu from "../PopularMenu/PopularMenu";
+import Testimonials from "../Testimonials/Testimonials";
 
 const Home = () => {
     return (
--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet} from 'react-helmet-async';
+import Cover from '../Shared/Cover/Cover';
+import menuImage from '../../assets/menu/banner3.jpg'
+import PopularMenu from '../Home/PopularMenu/PopularMenu';
 
 const Menu = () => {
     return (
@@ -6,7 +9,12 @@ const Menu = () => {
             <Helmet>
                 <title>Menu</title>
             </Helmet>
-            
+            <Cover img={menuImage} title={'our menu'}></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImage} title={'our menu'}></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImage} title={'our menu'}></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
