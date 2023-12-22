0d2f6485363ff15cf1235fd947b97725b5e2b7cd
rakibsarowar <rakibsarowar@gmail.com>
2023-12-22 23:30:04 +0600

    working on dashboard

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -7,6 +7,8 @@ import Menu from "../Pages/Menu/Menu/Menu";
 import Order from "../Pages/Order/Order/Order";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
+import Dashboard from "../Layout/DashBoard";
+import MyCart from "../Pages/DashBoard/MyCart/MyCart";
 
 
 
@@ -37,4 +39,14 @@ export const router = createBrowserRouter([
             },
         ]
     },
+    {
+        path: 'dashboard',
+        element: <Dashboard></Dashboard>,
+        children: [
+            {
+                path: 'mycart',
+                element: <MyCart></MyCart>
+            }
+        ]
+    }
 ]);
